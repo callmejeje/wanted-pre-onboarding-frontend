cd84e1e15304e604a8254b9f658802404d250729
callmejeje <choije401@naver.com>
2022-12-08 14:59:59 +0900

    feat: 이메일, 비밀번호 유효성 검사

---
+++ src/utils/validation.ts
@@ -0,0 +1,10 @@
+export function isValidEmail(email: string) {
+  const regExp = /\S+@\S+\.\S{2,3}$/;
+  const isValid = regExp.test(email);
+  return isValid;
+}
+
+export function isValidPassword(password: string) {
+  if (password.length >= 8) return true;
+  else return false;
+}
