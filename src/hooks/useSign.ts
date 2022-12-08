64d1567b0c589ff2dcaeac0f68548f5f41067ab3
callmejeje <choije401@naver.com>
2022-12-08 15:01:30 +0900

    feat: 로그인, 회원가입에 이용할 custom hook

---
+++ src/hooks/useSign.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { isValidEmail, isValidPassword } from "../utils/validation";
+
+function useSign() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailValid, setEmailValid] = useState(false);
+  const [passwordValid, setPasswordValid] = useState(false);
+
+  useEffect(() => {
+    setEmailValid(isValidEmail(email));
+  }, [email]);
+
+  useEffect(() => {
+    setPasswordValid(isValidPassword(password));
+  }, [password]);
+
+  return { email, setEmail, password, setPassword, emailValid, passwordValid };
+}
+
+export default useSign;
