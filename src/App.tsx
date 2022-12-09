e3c881d032fb6ba7cdd9097a7d5be7be147f0a75
callmejeje <choije401@naver.com>
2022-12-09 19:14:39 +0900

    feat: Context 추가, Todo페이지 이름 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { Route, Routes } from "react-router-dom";
+import ContextProvider from "./context/ContextProvider";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-import Todo from "./pages/Todo";
+import TodoList from "./pages/TodoList";
 
 function App() {
   return (
-    <div className="w-[100vw] h-[100vh]">
-      <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/todo" element={<Todo />} />
-      </Routes>
-    </div>
+    <ContextProvider>
+      <div className="w-[100vw] h-[100vh]">
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/todo" element={<TodoList />} />
+        </Routes>
+      </div>
+    </ContextProvider>
   );
 }
 
